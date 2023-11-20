b786c04122140663e12e7468015c197dc029ac92
Fragarias <148366640+Fragarias@users.noreply.github.com>
2023-11-20 15:05:30 +0000

    [Add] books_controller内アクション

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root :to => 'homes#top'
+  resources :books
+
+  # get 'books' => 'books#index'
+  # post 'books' => 'books#create'
+  # get 'books/new' => 'books#new'
+  # get 'books/:id/edit' => 'books#edit'
+  # get 'book' => 'books#show'
+  # patch 'books/:id' => 'books#update'
+  # delete 'books/:id' => 'books#destroy'
 end
