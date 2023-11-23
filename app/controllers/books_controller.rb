32d7c87a3ffecd625ada145e50e93f3020843af6
Fragarias <148366640+Fragarias@users.noreply.github.com>
2023-11-23 13:54:27 +0000

    [Add] バリデーションチェック

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,12 +6,13 @@ class BooksController < ApplicationController
 
   def create
     @book = Book.new(book_params)
-     @book.save
-      # flash[:notice] = "successfully"
+    if @book.save
+      flash[:notice] = "Book was successfully created."
       redirect_to book_path(@book.id)
-    # else
-    #   flash.now[:alert] = "error"
-    # end
+    else
+      @books = Book.all
+      render :index
+    end
   end
 
   def show
@@ -23,13 +24,21 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "Book was successfully updated."
+      redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
   end
 
   def destroy
     book = Book.find(params[:id])
-    book.destroy
+    if book.destroy
+      flash[:notice] = "Book was successfully destroyed."
+      redirect_to books_path
+    end
   end
 
   private
